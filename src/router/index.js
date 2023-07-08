15ba835474abca0dbcae6f081d54c2f9735fe280
EGNAR2023 <1384476@senati.pe>
2023-07-08 10:59:35 -0500

    Subir mi proyecto a GitHub

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
-//importamos vue-router//importamos vue-router
+//importamos vue-router
 import { createRouter, createWebHistory } from 'vue-router'
 //importamos los componentes de las vistas
 import Inicio from '@/views/Inicio.vue'
 import Registro from '@/views/Registro.vue'
+import IniciarSesion from '@/views/IniciarSesion.vue'
+import Dashboard from '@/views/Dashboard.vue'
 
 //creamos las rutas
 const routes = [
     {path: '/', name: 'Inicio', component: Inicio},
     {path: '/registro', name: 'Registro', component: Registro},
+    {path: '/iniciar-sesion', name: 'IniciarSesion', component : IniciarSesion},
+    {path: '/dashboard', name: 'Dashboard', component : Dashboard},
 ]
 
 //creamos el router
